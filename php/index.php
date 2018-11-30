557d16a8161663e9fde016564f04b91f1fd0879a
jakemurphy217 <jakemurphy217@gmail.com>
2018-11-30 10:48:07 +0000

    Add files via upload

--- php/index.php
+++ php/index.php
@@ -1,62 +1,64 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-  <title>Bootstrap 4 Example</title>
-  <meta charset="utf-8">
-  
-  <!-- so bootstrap can recongise between a mobile and destop device -->
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  
-  <!-- linking the bootstrap CSS CDN -->
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
-  
-  <!-- JQuery library -->
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-  
-  <!-- Popper JS -->
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
-  
-  <!-- Latest JavaScript -->
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
-    
-     <!-- my CSS -->
-  <link rel="stylesheet" href="style.css.css" type="text/css">
-    
+    <title>Bootstrap 4 Example</title>
+    <meta charset="utf-8">
+
+    <!-- so bootstrap can recongise between a mobile and destop device -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- linking the bootstrap CSS CDN -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+
+    <!-- JQuery library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+    <!-- Popper JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+
+    <!-- Latest JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+
+    <!-- my CSS -->
+    <link rel="stylesheet" href="style.css.css" type="text/css">
+
 </head>
+
 <body>
-    
+
     <nav class="navbar navbar-expand-lg navbar-light " style="background-color: aquamarine">
-        
-       <a class="navbar-brand" href="#" style="max-width: 30%;">
+
+        <a class="navbar-brand" href="#" style="max-width: 30%;">
             <img src="logo.png" class="img-fluid">InTheKitchen
-       </a>
-        
+        </a>
+
         <button class="navbar-toggler" data-toggle="collapse" data-target="#navbarMenu">
             <span class="navbar-toggler-icon"> </span>
         </button>
-        
-    <div class="collapse navbar-collapse" id="navbarMenu">
-        <ul class="navbar-nav ml-auto">
-            <li class="nav-item">
-                <a href="index.php" class="nav-link">Home</a>
-            </li>
-            
-             <li class="nav-item">
-                <a href="Favourites.php" class="nav-link">Favourites</a>
-            </li>
-            
-             <li class="nav-item">
-                <a href="Nearest.php" class="nav-link">Nearest</a>
-            </li>
-            
-             <li class="nav-item">
-                <a href="Account.php" class="nav-link">Account</a>
-            </li>
-        </ul>
-    </div>
-    
+
+        <div class="collapse navbar-collapse" id="navbarMenu">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a href="index.php" class="nav-link">Home</a>
+                </li>
+
+                <li class="nav-item">
+                    <a href="Favourites.php" class="nav-link">Favourites</a>
+                </li>
+
+                <li class="nav-item">
+                    <a href="Nearest.php" class="nav-link">Nearest</a>
+                </li>
+
+                <li class="nav-item">
+                    <a href="Account.php" class="nav-link">Account</a>
+                </li>
+            </ul>
+        </div>
+
     </nav>
-   <!-- end of navbar -->  
+    <!-- end of navbar -->
     <!-- start of jumbotron -->
     <div class="container-fluid">
         <div class="row">
@@ -72,95 +74,68 @@
     <div class="container-fluid my-container">
         <div class="row my row">
             <div class="col-md-8 col-sm-6 Search">
-                
-                <div class="input-group mb-3 Search-Box">
+
+                <!--  <div class="input-group mb-3 Search-Box">
                     <input  id="foodie" type="text" class="form-control" placeholder="what food are you craving?" aria-label="search box" aria-describedby="basic-addon2">
                         <div class="input-group-append">
-                            <button id="SearchBtn" class="btn btn-outline-secondary" type="button">Search</button>
-                        </div>
+                        
+                  -->
+                <div class="input-group mb-3 Search-Box">
+                    <form class="form">
+                        <label>Enter a Post ID</label>
+                        <input class="post-id-input" placeholder="what are you feeling?" aria-label="search box" aria-describedby="basic-addon2">
+                        <button type="submit" name="button" "btn btn-outline-secondary">Submit</button>
+                    </form>
+                    <div class="input-group-append">
+
+
+                    </div>
                 </div>
-                
-                
-              <script>
-                  
-                
-            
-        
-            var input = document.getElementById("foodie");
-            var searchBtn = document.getElementById("SearchBtn");
-            var value;
-                  
-          SearchBtn.addEventListener("click", () => {
-
-              value = input.value;
-
-              var url = "https://www.food2fork.com/api/search?key=99daf941fe57229da1083983707f7101&q=" + value +  "&page=2";
 
 
-               $.getJSON(url, function(data) 
-                
-                var recipies = data.recipies;
-                         var html= "<div>" 
-                         
-                         html+= "</div>"
-                var html = "<p>" + recipie.title + "</p>;
-            
-            
-                 console.log(data)
-               })
+                <div class="output"></div>
+
+                <!-- Handlebars template  -->
+                <script id="comments-template" type="text/x-handlebars-template">
+                    <ul>
+                        {{#each comments}}
+                    <li>{{image_url.body}}</li>
+                    <li>{{publisher.body}}</li>
+                    <li>{{f2f_url.body}}</li>
+                    <li>{{title.body}}</li>
+                    <li>{{source_url.body}}</li>
+                        {{/each}}
+                  </ul>
+                </script>
 
 
-          });
-        
-                  
-                  
-                  
-                  
-                  
-            
-                  
-                  
-                  
-                  
-                 
-            
-            // var button = select("#SearchBtn");
-            // button.mousePressed(foodss);
-                  
-            // input = select("#foodie")
-                  
-              // $(document).ready(function(){
-                 //  $("#SearchBtn").click(function(){
-                    //   $.getJSON(url1 + input.value() + url2, function(food){
-                    //   console.log(food);
-                //      
-                //  })
-                  //
-              // })
-            //      
-            //  })
-                  
-                  
-                   
-                  
-                  
-                
-              </script>
 
-            row 1 col 1
-            </div>
-            <div class="col-md-4 col-sm-6 Trending">
-            row 1 col 2
+
+
+
             </div>
         </div>
-    
+
     </div>
-    
-    
-    
-    
+
+
+
+
+
+    <!--
+                              Axios is a librays for sending ajax requests.
+                              link to documentaion: https://github.com/axios/axios
+                             -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.18.0/axios.js" charset="utf-8"></script>
+    <!--
+                              Handlebars is a client side template engine.
+                              link to documentaion:https://handlebarsjs.com/
+                             -->
+    <script src="handlebars-379172e.js" charset="utf-8"></script>
+    <script src="SearchAPI.js" charset="utf-8"></script>
 
 
 
 </body>
+
 </html>
